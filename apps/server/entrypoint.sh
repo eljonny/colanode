524dafd8ed5f27068f9a06bf59c9eeb2f00c904a
Jonathan Hyry <jonathan.hyry@outlook.com>
2025-07-23 13:51:30 -0700

    Fix a couple issues with the docker run and build
    The issues are as follows:
     - There has been an issue from the beginning with how
       `clouflared access tcp` is run where it occasionally needs manual
       login and needs manual login during the build
     - Fix issues with no trailing comment support in env files... I looked
       this up to make sure and thought it was supported, but it definitely
       is not
    
    These issues are corrected with this commit.

--- apps/server/entrypoint.sh
+++ apps/server/entrypoint.sh
@@ -1,4 +1,6 @@
-cloudflared access tcp --hostname colanode-redis.hyrysoftwarefoundation.org --url localhost:6379 &
-cloudflared access tcp --hostname colanode-pgsql.hyrysoftwarefoundation.org --url localhost:5432 &
+cloudflared access tcp --hostname colanode-redis.hyrysoftwarefoundation.org --url localhost:6379 \
+    --service-token-id $CFD_ACCESS_TOKEN_ID --service-token-secret $CFD_ACCESS_TOKEN_SECRET &
+cloudflared access tcp --hostname colanode-pgsql.hyrysoftwarefoundation.org --url localhost:5432 \
+    --service-token-id $CFD_ACCESS_TOKEN_ID --service-token-secret $CFD_ACCESS_TOKEN_SECRET &
 sleep 5
 node apps/server/dist/index.js
