fe4e922e15efc7694c3cb0bfaf1e84a34f6c7116
Jonathan Hyry <jonathan.hyry@outlook.com>
2025-07-24 00:30:42 -0700

    Improve docker image runtime flexibility for CFD Access client
    I'm making the dockerhub repo public so it should be usable for people
     that want to apply their own configurations.
    
    This commit does the following:
     - Adds new environment variables to the env files
     - Uses these new environment variables in the entrypoint script.

--- apps/server/entrypoint.sh
+++ apps/server/entrypoint.sh
@@ -1,6 +1,6 @@
-cloudflared access tcp --hostname colanode-redis.hyrysoftwarefoundation.org --url localhost:6379 \
-    --service-token-id $CFD_ACCESS_TOKEN_ID --service-token-secret $CFD_ACCESS_TOKEN_SECRET &
-cloudflared access tcp --hostname colanode-pgsql.hyrysoftwarefoundation.org --url localhost:5432 \
-    --service-token-id $CFD_ACCESS_TOKEN_ID --service-token-secret $CFD_ACCESS_TOKEN_SECRET &
+cloudflared access tcp --hostname $CFD_REDIS_HOST --url localhost:$CFD_REDIS_PORT \
+    --service-token-id $CFD_REDIS_ACCESS_TOKEN_ID --service-token-secret $CFD_REDIS_ACCESS_TOKEN_SECRET &
+cloudflared access tcp --hostname $CFD_PGSQL_HOST --url localhost:$CFD_PGSQL_PORT \
+    --service-token-id $CFD_PGSQL_ACCESS_TOKEN_ID --service-token-secret $CFD_PGSQL_ACCESS_TOKEN_SECRET &
 sleep 5
 node apps/server/dist/index.js
